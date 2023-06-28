3ef419d1a522c4ec38f9ac5b2f6871a49c1115d6
Ebenezer Exetie <104176127+Eenrics@users.noreply.github.com>
2023-06-28 13:13:18 +0000

    exe 12.15 done

--- todo-app/todo-frontend/run_dev_docker.sh
+++ todo-app/todo-frontend/run_dev_docker.sh
@@ -1,3 +1,5 @@
-sudo docker build -f ./dev.Dockerfile -t front-dev .
+# sudo docker build -f ./dev.Dockerfile -t front-dev .
 
-sudo docker run -p 3000:3000 -v "$(pwd):/usr/src/app/" front-dev
+# sudo docker run -p 3000:3000 -v "$(pwd):/usr/src/app/" front-dev
+
+sudo docker compose -f ./docker-compose.dev.yml up
